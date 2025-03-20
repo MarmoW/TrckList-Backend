c705732af4c5f7583b977f7c8ccff8d88984e620
MarmoW <115847012+MarmoW@users.noreply.github.com>
2025-03-20 11:57:41 -0300

    feat: db tables and ts config

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client';
-import dayjs from 'dayjs';
+
+
 const prisma = new PrismaClient();
 
 async function main() {
     const users = await prisma.user.findMany();
     console.log(users);
+
   };
+
 
 main()
   .catch((e) => {
